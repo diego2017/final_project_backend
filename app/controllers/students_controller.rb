6da0476a04f88d4b6202e68900ac95706bf31277
Diego Villanueva <diego_v_f@hotmail.com>
2017-05-04 15:17:07 +1000

    udpate bio front and back end working

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -24,4 +24,51 @@ class StudentsController < ApplicationController
     end
     render json: {message: "success"}
   end
+
+
+
+  def update
+    @student = Student.find(params[:id])
+    newBio = params[:bio]
+
+    # change the bio
+    @student.bio = newBio
+
+
+    # save student with new data
+    if !@student.save
+      render json: {message: "could not save record"}, status: :bad_request
+    else
+      render json: {message: "success"}
+    end
+
+    # binding.pry
+  #
+  #   # This is the magic stuff that will let us upload an image to Cloudinary when creating a new animal.
+  #   # First, check to see if the user has attached an image for uploading
+  #   if params[:file].present?
+  #     # Then call Cloudinary's upload method, passing in the file in params
+  #     req = Cloudinary::Uploader.upload(params[:file])
+  #     # Using the public_id allows us to use Cloudinary's image transformation methods.
+  #     @place.image = req["public_id"]
+  #     @place.save
+  #   end
+  #
+  #   # update features
+  #   # first delete all associations
+  #   @place.features.delete_all
+  #   # then recreate each feature association that was checked in the form
+  #   params[:features].each do |feature_id|
+  #     @place.features << Feature.find( feature_id )
+  #   end
+  #
+  #   # @place.user = @current_user
+  #
+  #   if @place.update_attributes(clean_params)
+  #     redirect_to controller: "places", action: "show", place_id: @place.id
+  #   else
+  #     render :edit
+  #   end
+  end
+
 end
